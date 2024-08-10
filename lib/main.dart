688414a7ab31b5d641f01340115f6bef7619078e
ArpitAswal <arpitaswal995@gmail.com>
2024-08-10 21:34:02 +0530

    update commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,119 @@
+
+import 'package:VideoCaller/videocall_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:video_calling/videocall_screen.dart';
 
-const String appId = "1be0fa04f96344838da35ee0cb798d63";
+void main() => runApp(const MyApp());
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
-  runApp(const MaterialApp(
-      debugShowCheckedModeBanner: false, home:
-  MyApp()
-  ));
-}
-
-class MyApp extends StatefulWidget {
-
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-         body: Stack(
-           alignment: Alignment.bottomCenter,
-           children: [
-             Container(
-                 height:MediaQuery.of(context).size.height,
-                 width:MediaQuery.of(context).size.width,
-                 //color:const Color(0xff023bf6)
-             color:Colors.white
-             ),
-             Positioned(
-               bottom: 300,
-               child: SizedBox(
-                 height: 380,
-                 width: 380,
-                 child:Image.asset('assets/images.jpg',fit: BoxFit.contain,)
-               ),
-             ),
-             Container(
-               height:MediaQuery.of(context).size.height/4,
-               width:MediaQuery.of(context).size.width,
-               decoration: const BoxDecoration(
-                 borderRadius: BorderRadius.vertical(top: Radius.circular(34)),
-                 color: Colors.white
-               ),
-               child: Column(
-                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                 crossAxisAlignment: CrossAxisAlignment.center,
-                 children: [
-                 const Text('WELCOME',style:TextStyle(color:Colors.black,fontWeight: FontWeight.bold,fontSize: 28)),
-                 Row(
-                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                 crossAxisAlignment: CrossAxisAlignment.center,
-                 children:[  ElevatedButton(
-                     onPressed: (){
-                       Navigator.push(context, MaterialPageRoute(builder: (context)=> VideoCallScreen()));
-                     },
-                   style: ButtonStyle(
-                       minimumSize: MaterialStateProperty.all(const Size(110, 50)),
-                     backgroundColor: MaterialStateProperty.all(const Color(0xff023bf6)),
-                     shadowColor: MaterialStateProperty.all(Colors.tealAccent),
-                     elevation:  MaterialStateProperty.all(6),
-                     shape:  MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)))
-                   ),
-                     child: const Center(child: Text('JOIN',style: TextStyle(color:Colors.white,fontSize: 26,fontWeight: FontWeight.w500),)),
-                   ),
-                   ElevatedButton(
-                     onPressed: (){
-                       SystemNavigator.pop();
-                     },
-                     style: ButtonStyle(
-                       minimumSize: MaterialStateProperty.all(const Size(110, 50)),
-                         backgroundColor: MaterialStateProperty.all(Colors.redAccent),
-                         shadowColor: MaterialStateProperty.all(Colors.tealAccent),
-                         elevation:  MaterialStateProperty.all(6),
-                         shape:  MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)))),
-                     child: const Center(child: Text('EXIT',style: TextStyle(color:Colors.white,fontWeight: FontWeight.w500,fontSize: 26),)),
-                   )
-                 ]
-                 ),
-
-                 ],
-               ),
-               ),
-
-           ],
-         ),
+    return MaterialApp(
+      title: 'Agora Group Video Call',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          color: Colors.white
+        ),
+        primarySwatch: Colors.blue,
+        hintColor: Colors.blueAccent,
+      ),
+      home: const MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final myController = TextEditingController();
+  bool _validateError = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Conference Caller'),
+        elevation: 8,
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: height,
+          width: width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                  height: height * .6,
+                  width: width,
+                  child:Image.asset('assets/online-meeting.jpg',fit: BoxFit.cover, filterQuality: FilterQuality.high,)
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
+                child: SizedBox(
+                  width: width,
+                  child: TextFormField(
+                    controller: myController,
+                    decoration: InputDecoration(
+                      labelText: 'Channel Name',
+                      labelStyle: const TextStyle(color: Colors.blue),
+                      errorText: _validateError
+                          ? 'Channel name is mandatory'
+                          : null,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: width * .25,
+                child: MaterialButton(
+                  onPressed: onJoin,
+                  height: 40,
+                  color: Colors.blueAccent,
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Join', style: TextStyle(color: Colors.white)),
+                      SizedBox(width: 6.0),
+                      Icon(Icons.arrow_forward, color: Colors.white),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height:100.0)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> onJoin() async {
+    setState(() {
+      myController.text.isEmpty
+          ? _validateError = true
+          : _validateError = false;
+    });
+    if(!_validateError){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context)=> VideoCallScreen(channelName: myController.text.toString())));
+    }
+  }
+}
---
+++ lib/utils.dart
@@ -0,0 +1,16 @@
+
+import 'dart:core';
+
+class Utils{
+
+
+// Fill the app ID of your project generated on Agora Console.
+  static const appID = "1be0fa04f96344838da35ee0cb798d63";
+
+// Fill the channel name.
+  static const channelName = "Online Meeting";
+
+// Fill the temp token generated on Agora Console.
+  static const token = "007eJxTYGAoanRf/PjzxFLB/24Mxl++e6b4rT23TvFRQevia6f4P21QYDBMSjVISzQwSbM0MzYxsTC2SEk0Nk1NNUhOMre0SDEzDvXbntYQyMhwp281AyMUgvh8DP55OZl5qQq+qaklmXnpDAwAMfglHg==";
+
+}
